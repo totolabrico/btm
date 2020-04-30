d8b6fcd91ea02e46a6d153a78212861819ffbb7b
totolabricot <thomas-barbe@hotmail.com>
2020-04-30 17:21:39 +0200

    rename back again

--- python/draw_menu.py
+++ python/draw_menu.py
@@ -5,65 +5,64 @@ class Draw_menu:
     def __init__(self,Menu):
         self.menu=Menu
         #self.navigator=self.machine.navigator
-        self.pointer_setting=0
-        self.pointer_setting_display=0
-        self.origin_setting=0
-        self.max_setting=4
-        self.list_setting=[]
-        self.set_draw_setting()
+        self.pointer=0
+        self.pointer_display=0
+        self.origin=0
+        self.max=4
+        self.list=[]
+        self.set_draw()
 
-    def set_draw_setting(self):
-        self.set_pointer_setting()
-        self.set_list_setting()
+    def set_draw(self):
+        self.set_pointer()
+        self.set_list()
 
-    def set_max_setting(self):
+    def set_max(self):
         if self.menu.name=="tracks" or self.menu.name=="notes":
-            self.max_setting=1
+            self.max=1
         else:
-            self.max_setting=4
-        if len(self.menu.list_setting)<self.max_setting:
-            self.max_setting=len(self.menu.list_setting)
+            self.max=4
+        if len(self.menu.list)<self.max:
+            self.max=len(self.menu.list)
 
-    def set_list_setting(self):
-        self.set_max_setting()
-        self.list_setting=[]
+    def set_list(self):
+        self.set_max()
+        self.list=[]
 
         if self.menu.name=="tracks" or self.menu.name=="notes":
-            if len(self.menu.list_setting)>0:
-                self.list_setting.append(self.menu.list_setting[self.menu.pointer_setting])
+            if len(self.menu.list)>0:
+                self.list.append(self.menu.list[self.menu.pointer])
 
         else:
-            i=self.origin_setting
+            i=self.origin
 
-            while i<self.origin_setting+self.max_setting:
-                to=self.menu.list_setting[i]
+            while i<self.origin+self.max:
+                to=self.menu.list[i]
                 if self.menu.name=="browser" and os.path.isdir(self.menu.path+to)==True:
                     to=">"+to
-                self.list_setting.append(to)
+                self.list.append(to)
                 i+=1
 
-    def set_pointer_setting(self):# set pointer & origin
+    def set_pointer(self):# set pointer & origin
 
-        if self.menu.pointer_setting==0: # if 'min'
-            self.origin_setting=0
-            self.pointer_setting_display=0
+        if self.menu.pointer==0: # if 'min'
+            self.origin=0
+            self.pointer_display=0
 
-        elif self.menu.pointer_setting==len(self.menu.list_setting)-1: # if 'max'
-            self.pointer_setting_display=self.max_setting-1
-            self.origin_setting=len(self.menu.list_setting)-self.max_setting
+        elif self.menu.pointer==len(self.menu.list)-1: # if 'max'
+            self.pointer_display=self.max-1
+            self.origin=len(self.menu.list)-self.max
 
-        elif self.menu.pointer_setting>self.pointer_setting:# if '+'
-            if self.pointer_setting<self.max_setting-1:
-                self.pointer_setting_display+=1
-            elif self.pointer_setting_display==self.max_setting-1:
-                self.origin_setting+=1
+        elif self.menu.pointer>self.pointer:# if '+'
+            if self.pointer<self.max-1:
+                self.pointer_display+=1
+            elif self.pointer_display==self.max-1:
+                self.origin+=1
 
-        elif self.menu.pointer_setting<self.pointer_setting:# if '-'
-            if self.pointer_setting_display>0:
-                self.pointer_setting_display-=1
-            elif self.pointer_setting_display==0:
-                self.origin_setting-=1
+        elif self.menu.pointer<self.pointer:# if '-'
+            if self.pointer_display>0:
+                self.pointer_display-=1
+            elif self.pointer_display==0:
+                self.origin-=1
 
-        print("pointer_setting",self.pointer_setting,self.menu.pointer_setting,self.origin_setting,self.pointer_setting_display)
-        self.pointer_setting=self.menu.pointer_setting
-
+        print("pointer",self.pointer,self.menu.pointer,self.origin,self.pointer_display)
+        self.pointer=self.menu.pointer
--- python/draw_menu_editor.py
+++ python/draw_menu_editor.py
@@ -5,42 +5,42 @@ class Draw_menu_editor(Draw_menu):
     def __init__(self,Menu):
         self.menu=Menu
         #self.navigator=self.machine.navigator
-        self.pointer_element=0
-        self.pointer_element_display=0
-        self.origin_element=0
-        self.max_element=4
-        self.list_element=[]
-        self.set_draw_element()
+        self.pointer=0
+        self.pointer_display=0
+        self.origin=0
+        self.max=4
+        self.list=[]
+        self.set_draw()
 
-    def set_draw_element(self):
-        self.set_pointer_element()
-        self.set_list_element()
+    def set_draw(self):
+        self.set_pointer()
+        self.set_list()
 
-    def set_list_element(self):
-        self.list_element=[[],[]]
-        self.origin_element=int(self.menu.element/(self.menu.element_per_line*2))*(self.menu.element_per_line*2)
-        max_element=self.origin_element+self.menu.element_per_line*2
-        if max_element>self.menu.nb_element:
-            max_element=self.menu.nb_element
+    def set_list(self):
+        self.list=[[],[]]
+        self.origin=int(self.menu.element/(self.menu.element_per_line*2))*(self.menu.element_per_line*2)
+        max=self.origin+self.menu.element_per_line*2
+        if max>self.menu.nb:
+            max=self.menu.nb
 
-        self.set_pointer_element(self.origin_element)
-        value=self.origin_element
+        self.set_pointer(self.origin)
+        value=self.origin
         i=0
         j=0
-        while value<max_element:
-            self.list_element[j].append(self.menu.list_element[value])
+        while value<max:
+            self.list[j].append(self.menu.list[value])
             value+=1
             i+=1
             if i!=0 and i%self.menu.element_per_line==0:
                 i=0
                 j+=1
-        print("pointer_element",self.pointer_element)
+        print("pointer",self.pointer)
 
-    def set_pointer_element(self,Min):
+    def set_pointer(self,Min):
         pointer_display=self.menu.element-Min
-        if pointer_element<self.menu.element_per_line:
+        if pointer<self.menu.element_per_line:
             j=0
         else:
             j=1
-            pointer_element-=self.menu.element_per_line
-        self.pointer_element=[pointer_element,j]
+            pointer-=self.menu.element_per_line
+        self.pointer=[pointer,j]
