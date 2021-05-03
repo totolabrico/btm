4a5328d1aae95a94c407e4008602f379c7474c15
totolabricot <thomas-barbe@hotmail.com>
2021-05-03 14:53:04 +0200

    newpush

--- python/menu.py
+++ python/menu.py
@@ -6,12 +6,12 @@ import os, sys
 
 
 class Menu():
-	
+
 	def __init__(self,Navigator):
 		self.navigator=Navigator
 		self.tools=menu_tools.copy()
 		self.pointer=0
-		
+
 	def set_pointer(self):
 		self.pointer=int(self.tools["pointer"][0]+self.tools["pointer"][1]*self.tools["grid"][0])
 
@@ -23,8 +23,8 @@ class Menu():
 			self.navigator.set_menu(self.list[self.pointer])
 		elif cmd=="back":
 			self.navigator.set_menu(self.mom)
-				
-	def draw(self): 
+
+	def draw(self):
 		draw_title(self.name)
 
 
@@ -33,16 +33,20 @@ class Editor():
 		self.machine=Machine
 		self.partition=Partition
 		self.parameters=List
-			
+
 	def sort(self,cmd,arg):
 		print("editot sort")
 		if cmd=="edit":
-			self.parameters[self.pointer]=edit(arg,self.parameters[self.pointer])
-			self.send_osc()
+			parameter=self.parameters[self.pointer]
+			if parameter[0]=="sample":
+				self.navigator.set_menu("sample")
+			else:
+				parameter=edit(arg,parameter)
+				self.send_osc()
 			#self.machine.save_set()
-	
+
 	def set_list(self):
-		self.list=[]		
+		self.list=[]
 		for parameter in self.parameters:
 			self.list.append(parameter[0]+":"+setting_to_string(parameter[1]))
 		return self.list
@@ -55,22 +59,21 @@ class Browser(): # != Editor un browser est un menu qui permet de se deplacer da
 
 	def __init__(self,Path):
 		self.path=Path
-		
+
 	def set_list(self):
 		self.list=[]
 		for element in os.listdir(self.path):
 			if element[:1]!=".":
 				self.list.append(element)
 		return self.list
-       
-        
+
 	def sort(self,cmd,arg):
 		if cmd=="move":
 			if arg[0]=="y":
 				self.tools=move(arg[0],arg[1],self.tools,len(self.list))
 			if arg[0]=="x":
 				self.set_path(arg[1])
-    
+
 	def set_path(self,cmd):
 		cut=self.path.split("/")
 		last_word=cut[len(cut)-1]
@@ -89,4 +92,3 @@ class Browser(): # != Editor un browser est un menu qui permet de se deplacer da
 				if element==last_word:
 					self.pointer=i
 					i+=1
-            
